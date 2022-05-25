3cbf48edef1b995a43d165ef2205ef2c2b5e0b62
Prakasa <nedya.prakasa@tokopedia.com>
2022-05-25 07:33:01 +0700

    feat: add backup scripts

---
+++ scripts/backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Please contact administrator first before you can use this command without trouble"
+
+scp root@pangkalpinang:/etc/nginx/conf.d/pn-pangkalpinang.go.id.conf pn-pangkalpinang.go.id.conf
+scp root@pangkalpinang:/etc/nginx/conf.d/www.pn-pangkalpinang.go.id.conf www.pn-pangkalpinang.go.id.conf
